dc545e2b194ed67838f0b139c8dd52cf8beb3b87
dannybowmann <dannybowman80@gmail.com>
2023-09-19 10:15:48 -0400

    finished playRound function now stuck on game func loop, only runs once

--- firstcode.js
+++ firstcode.js
@@ -9,30 +9,60 @@ function getComputerChoice() {
     }
 }
 
-function playRound(PlayerSelection, ComputerSelection) {
-    switch () {
-        case (PlayerSelection === 'rock' && ComputerSelection === 'paper'):
-            return 'You Lose! Paper Beats Rock!';
-            break;
-        case (PlayerSelection === 'rock' && ComputerSelection === 'scissors'):
-            return 'You Win! Rock Beats Beats!';
-             break;
-        case (PlayerSelection === 'paper' && ComputerSelection === 'rock'):
-            return 'You Win! Paper Beats Rock!';
-            break;   
-        case (PlayerSelection === 'paper' && ComputerSelection === 'scissors'):
-            return 'You Lose! Scissors Beat Paper!';
-             break; 
-        case (PlayerSelection === 'scissors' && ComputerSelection === 'paper'):
-            return 'You Win! Scissors Beat Paper!';
-            break;  
-        case (PlayerSelection === 'scissors' && ComputerSelection === 'rock'):
-            return 'You Lose! Rock Beats Scissors!';
-            break;     
-        case (PlayerSelection === ComputerSelection):
-            return 'Tie!'
-            break
-        default:
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+    return 'You Lose! Paper Beats Rock!';
+        } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You Win! Rock Beats Scissors!';
+        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You Win! Paper Beats Rock!';
+        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You Lose! Scissors Beat Paper!';
+        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You Win! Scissors Beat Paper!';
+        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You Lose! Rock Beats Scissors!';
+        } else if (playerSelection === computerSelection) {
+        return 'Tie!'
+        };
+}       
+
+function game(n) {
+    for (let i = 0; i < n; i++) {
+        playRound(playerSelection, computerSelection);
     }
 }
-console.log(playRound('rock', 'paper'))
+
+let playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase()
+let computerSelection = getComputerChoice()
+console.log(playRound(playerSelection, computerSelection))
+game(5)
+
+
+
+
+
+/* switch () {
+    case (PlayerSelection === 'rock' && ComputerSelection === 'paper'):
+        return 'You Lose! Paper Beats Rock!';
+        break;
+    case (PlayerSelection === 'rock' && ComputerSelection === 'scissors'):
+        return 'You Win! Rock Beats Beats!';
+         break;
+    case (PlayerSelection === 'paper' && ComputerSelection === 'rock'):
+        return 'You Win! Paper Beats Rock!';
+        break;   
+    case (PlayerSelection === 'paper' && ComputerSelection === 'scissors'):
+        return 'You Lose! Scissors Beat Paper!';
+         break; 
+    case (PlayerSelection === 'scissors' && ComputerSelection === 'paper'):
+        return 'You Win! Scissors Beat Paper!';
+        break;  
+    case (PlayerSelection === 'scissors' && ComputerSelection === 'rock'):
+        return 'You Lose! Rock Beats Scissors!';
+        break;     
+    case (PlayerSelection === ComputerSelection):
+        return 'Tie!'
+        break
+    default:
+} */
