9a4fbb0860d799868db80aeb18393e2205e84274
dannybowmann <dannybowman80@gmail.com>
2023-09-19 10:57:20 -0400

    realized my mistake with for loop; corrected and finished project. (typeError I don't understand as code runs just fine)

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # first_js_code
+My first JavaScript Program from scratch!!!
+rock paper scissors
+pray for me
+
+It works!!!
--- firstcode.js
+++ firstcode.js
@@ -25,44 +25,37 @@ function playRound(playerSelection, computerSelection) {
         } else if (playerSelection === computerSelection) {
         return 'Tie!'
         };
-}       
-
-function game(n) {
-    for (let i = 0; i < n; i++) {
-        playRound(playerSelection, computerSelection);
-    }
 }
 
-let playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase()
-let computerSelection = getComputerChoice()
-console.log(playRound(playerSelection, computerSelection))
-game(5)
-
-
-
-
-
-/* switch () {
-    case (PlayerSelection === 'rock' && ComputerSelection === 'paper'):
-        return 'You Lose! Paper Beats Rock!';
-        break;
-    case (PlayerSelection === 'rock' && ComputerSelection === 'scissors'):
-        return 'You Win! Rock Beats Beats!';
-         break;
-    case (PlayerSelection === 'paper' && ComputerSelection === 'rock'):
-        return 'You Win! Paper Beats Rock!';
-        break;   
-    case (PlayerSelection === 'paper' && ComputerSelection === 'scissors'):
-        return 'You Lose! Scissors Beat Paper!';
-         break; 
-    case (PlayerSelection === 'scissors' && ComputerSelection === 'paper'):
-        return 'You Win! Scissors Beat Paper!';
-        break;  
-    case (PlayerSelection === 'scissors' && ComputerSelection === 'rock'):
-        return 'You Lose! Rock Beats Scissors!';
-        break;     
-    case (PlayerSelection === ComputerSelection):
-        return 'Tie!'
-        break
-    default:
-} */
+function game() {
+    for (a = 0, b = 0; a < 3 && b < 3; a, b) {
+        let playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase()
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+        if (playerSelection === computerSelection) {
+            a = a;
+            b = b;
+        } else if ((playerSelection === 'rock' && computerSelection === 'scissors') 
+        || (playerSelection === 'paper' && computerSelection === 'rock') 
+        || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+            a += 1;
+        } else if ((playerSelection === 'rock' && computerSelection === 'paper')
+            || (playerSelection === 'paper' && computerSelection === 'scissors') 
+            || (playerSelection === 'scissors' && computerSelection === 'rock')) {
+                b += 1;
+            } else if(playerSelection == null) {
+                console.log('You dont want to play?')
+            } else {
+                console.log('incorrect value, try again.')
+            };
+            console.log('PS score =' + '' + a);
+            console.log('CS score =' + '' + b);
+        
+    }
+}
+let a;
+let b;
+let playerSelection;
+let computerSelection;
+game()
