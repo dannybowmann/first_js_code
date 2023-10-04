69341ca27d92c574a3fa360fb10b4d881fdd2732
dannybowmann <127047876+dannybowmann@users.noreply.github.com>
2023-10-04 16:01:05 -0400

    Update firstcode.js

--- firstcode.js
+++ firstcode.js
@@ -1,3 +1,150 @@
+const btn1 = document.querySelector('#btn1')
+const btn2 = document.querySelector('#btn2')
+const btn3 = document.querySelector('#btn3')
+const text = document.querySelector('.text')
+const text2 = document.querySelector('.text2')
+const body = document.querySelector('body')
+let a = 0
+    let b = 0
+
+
+ btn1.addEventListener('click', (playerSelection, computerSelection) => {
+    playerSelection = 'rock'
+    computerSelection = getComputerChoice()
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+        text.textContent = 'You Lose! Paper Beats Rock!';
+            } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+                text.textContent = 'You Win! Rock Beats Scissors!';
+            } else if (playerSelection === computerSelection) {
+                text.textContent = 'Tie!'
+            };
+    if (playerSelection === computerSelection) {
+         a = a;
+         b = b;
+    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') 
+        || (playerSelection === 'paper' && computerSelection === 'rock') 
+        || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        a += 1;
+    } else if ((playerSelection === 'rock' && computerSelection === 'paper')
+        || (playerSelection === 'paper' && computerSelection === 'scissors') 
+        || (playerSelection === 'scissors' && computerSelection === 'rock')) {
+        b += 1;
+    }
+    text2.textContent = `Your Score: ${a} \n\nComputer Score: ${b}`
+    if (a === 3) {
+        text2.textContent = 'GAME OVER     YOU WIN!!'
+        btn1.disabled = true
+        btn2.disabled = true
+        btn3.disabled = true
+        let reset = document.createElement('button')
+        reset.setAttribute('id', 'reset')
+        reset.textContent = 'Play Again'
+        body.appendChild(reset)
+        reset.onclick = () => location.reload()
+    } else if (b === 3) {
+        text2.textContent = 'GAME OVER     YOU LOSE!!'
+        btn1.disabled = true
+        btn2.disabled = true
+        btn3.disabled = true
+        let reset = document.createElement('button')
+        reset.setAttribute('id', 'reset')
+        reset.textContent = 'Play Again'
+        body.appendChild(reset)
+        reset.onclick = () => location.reload()
+    }
+ })
+ btn2.addEventListener('click', (playerSelection, computerSelection) => {
+    playerSelection = 'paper'
+    computerSelection = getComputerChoice()
+    if (playerSelection === 'paper' && computerSelection === 'rock') {
+        text.textContent = 'You Win! Paper Beats Rock!';
+        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        text.textContent = 'You Lose! Scissors Beat Paper!';
+        } else if (playerSelection === computerSelection) {
+        text.textContent = 'Tie!'
+        };
+    if (playerSelection === computerSelection) {
+        a = a;
+        b = b;
+    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') 
+        || (playerSelection === 'paper' && computerSelection === 'rock') 
+        || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        a += 1;
+    } else if ((playerSelection === 'rock' && computerSelection === 'paper')
+        || (playerSelection === 'paper' && computerSelection === 'scissors') 
+        || (playerSelection === 'scissors' && computerSelection === 'rock')) {
+        b += 1;
+    }
+    text2.textContent = `Your Score: ${a} \n\nComputer Score: ${b}`
+    if (a === 3) {
+        text2.textContent = 'GAME OVER     YOU WIN!!'
+        btn1.disabled = true
+        btn2.disabled = true
+        btn3.disabled = true
+        let reset = document.createElement('button')
+        reset.setAttribute('id', 'reset')
+        reset.textContent = 'Play Again'
+        body.appendChild(reset)
+        reset.onclick = () => location.reload()
+    } else if (b === 3) {
+        text2.textContent = 'GAME OVER     YOU LOSE!!'
+        btn1.disabled = true
+        btn2.disabled = true
+        btn3.disabled = true
+        let reset = document.createElement('button')
+        reset.setAttribute('id', 'reset')
+        reset.textContent = 'Play Again'
+        body.appendChild(reset)
+        reset.onclick = () => location.reload()
+    }
+ })
+
+ btn3.addEventListener('click', (playerSelection, computerSelection) => {
+    playerSelection = 'scissors'
+    computerSelection = getComputerChoice()
+    if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        text.textContent = 'You Win! Scissors Beat Paper!';
+        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        text.textContent = 'You Lose! Rock Beats Scissors!';
+        } else if (playerSelection === computerSelection) {
+        text.textContent = 'Tie!'
+        };
+    if (playerSelection === computerSelection) {
+        a = a;
+        b = b;
+    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') 
+        || (playerSelection === 'paper' && computerSelection === 'rock') 
+        || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        a += 1;
+    } else if ((playerSelection === 'rock' && computerSelection === 'paper')
+        || (playerSelection === 'paper' && computerSelection === 'scissors') 
+        || (playerSelection === 'scissors' && computerSelection === 'rock')) {
+        b += 1;
+    }
+    text2.textContent = `Your Score: ${a} \n\nComputer Score: ${b}`
+    if (a === 3) {
+        text2.textContent = 'GAME OVER     YOU WIN!!'
+        btn1.disabled = true
+        btn2.disabled = true
+        btn3.disabled = true
+        let reset = document.createElement('button')
+        reset.setAttribute('id', 'reset')
+        reset.textContent = 'Play Again'
+        body.appendChild(reset)
+        reset.onclick = () => location.reload()
+    } else if (b === 3) {
+        text2.textContent = 'GAME OVER     YOU LOSE!!'
+        btn1.disabled = true
+        btn2.disabled = true
+        btn3.disabled = true
+        let reset = document.createElement('button')
+        reset.setAttribute('id', 'reset')
+        reset.textContent = 'Play Again'
+        body.appendChild(reset)
+        reset.onclick = () => location.reload()
+    }
+ })
+ 
 function getComputerChoice() {
     let x = Math.random()
     if (x <= .34) {
@@ -9,53 +156,3 @@ function getComputerChoice() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'rock' && computerSelection === 'paper') {
-    return 'You Lose! Paper Beats Rock!';
-        } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return 'You Win! Rock Beats Scissors!';
-        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return 'You Win! Paper Beats Rock!';
-        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return 'You Lose! Scissors Beat Paper!';
-        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return 'You Win! Scissors Beat Paper!';
-        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return 'You Lose! Rock Beats Scissors!';
-        } else if (playerSelection === computerSelection) {
-        return 'Tie!'
-        };
-}
-
-function game() {
-    for (a = 0, b = 0; a < 3 && b < 3; a, b) {
-        let playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase()
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-        if (playerSelection === computerSelection) {
-            a = a;
-            b = b;
-        } else if ((playerSelection === 'rock' && computerSelection === 'scissors') 
-        || (playerSelection === 'paper' && computerSelection === 'rock') 
-        || (playerSelection === 'scissors' && computerSelection === 'paper')) {
-            a += 1;
-        } else if ((playerSelection === 'rock' && computerSelection === 'paper')
-            || (playerSelection === 'paper' && computerSelection === 'scissors') 
-            || (playerSelection === 'scissors' && computerSelection === 'rock')) {
-                b += 1;
-            } else if(playerSelection == null) {
-                console.log('You dont want to play?')
-            } else {
-                console.log('incorrect value, try again.')
-            };
-            console.log('PS score =' + '' + a);
-            console.log('CS score =' + '' + b);
-        
-    }
-}
-let a;
-let b;
-let playerSelection;
-let computerSelection;
-game()
